950f6cdb8994d4e67a081ae8eec112f2bcab08e5
Andrii Kurdiumov <kant2002@gmail.com>
2026-03-26 17:09:31 +0100

    Create MSBuild test project for create project task (#33)
    Related #32

---
+++ Tests/Confuser.MSBuild.Tasks.Tests/CreateProjectTests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Confuser.UnitTest;
+using Microsoft.Build.Framework;
+using Moq;
+using Xunit;
+
+namespace Confuser.MSBuild.Tasks.Tests {
+	public class CreateProjectTests {
+		private readonly ITestOutputHelper outputHelper;
+		private Mock<IBuildEngine> buildEngine;
+		private List<BuildErrorEventArgs> errors;
+
+		public CreateProjectTests(ITestOutputHelper outputHelper) {
+			this.outputHelper = outputHelper ?? throw new ArgumentNullException(nameof(outputHelper));
+			this.buildEngine = new Mock<IBuildEngine>();
+			this.errors = new List<BuildErrorEventArgs>();
+			this.buildEngine.Setup(x => x.LogErrorEvent(It.IsAny<BuildErrorEventArgs>())).Callback<BuildErrorEventArgs>(e => errors.Add(e));
+		}
+
+
+
+		[Fact]
+		[Trait("Category", "MSBuildIntegration")]
+		public void CreateNewProjectIfNoSourceProject() {
+			var assembly = new Mock<ITaskItem>();
+			assembly.SetupAllProperties();
+			assembly.Object.ItemSpec = $".\\bin\\debug\\test.dll";
+			var baseDirectory = new Mock<ITaskItem>();
+			baseDirectory.SetupAllProperties();
+			baseDirectory.Object.ItemSpec = $".";
+			var resultProject = new Mock<ITaskItem>();
+			resultProject.SetupAllProperties();
+			resultProject.Object.ItemSpec = $"result-empty.crproj";
+
+			var task = new CreateProjectTask();
+			task.AssemblyPath = assembly.Object;
+			task.BuildEngine = buildEngine.Object;
+			task.ResultProject = resultProject.Object;
+			task.References = Array.Empty<ITaskItem>();
+
+			//Act
+			var success = task.Execute();
+
+			//Assert
+			Assert.True(success);
+			Assert.Empty(errors);
+			Assert.True(File.Exists(task.ResultProject.ItemSpec));
+			Assert.Collection(
+				File.ReadLines(".\\Resources\\confuser-empty.expected.crproj"),
+				File.ReadLines(task.ResultProject.ItemSpec)
+					.Select<string, Action<string>>((string actual) => (string expected) => Assert.Equal(expected, actual))
+					.ToArray()
+			);
+		}
+
+		[Fact]
+		[Trait("Category", "MSBuildIntegration")]
+		public void BaseDirectoryOverridenTest() {
+			var sourceProject = new Mock<ITaskItem>();
+			sourceProject.SetupAllProperties();
+			sourceProject.Object.ItemSpec = $".\\Resources\\confuser.src.crproj";
+			var assembly = new Mock<ITaskItem>();
+			assembly.SetupAllProperties();
+			assembly.Object.ItemSpec = $".\\bin\\debug\\test.dll";
+			var baseDirectory = new Mock<ITaskItem>();
+			baseDirectory.SetupAllProperties();
+			baseDirectory.Object.ItemSpec = $".";
+			var resultProject = new Mock<ITaskItem>();
+			resultProject.SetupAllProperties();
+			resultProject.Object.ItemSpec = $"result.crproj";
+
+			var task = new CreateProjectTask();
+			task.AssemblyPath = assembly.Object;
+			task.SourceProject = sourceProject.Object;
+			task.BuildEngine = buildEngine.Object;
+			task.ResultProject = resultProject.Object;
+			task.References = Array.Empty<ITaskItem>();
+
+			//Act
+			var success = task.Execute();
+
+			//Assert
+			Assert.True(success);
+			Assert.Empty(errors);
+			Assert.True(File.Exists(task.ResultProject.ItemSpec));
+			Assert.Collection(File.ReadLines(task.ResultProject.ItemSpec),
+				File.ReadLines(".\\Resources\\confuser.expected.crproj")
+					.Select<string, Action<string>>((string actual) => (string expected) => Assert.Equal(expected, actual))
+					.ToArray()
+			);
+		}
+	}
+}
